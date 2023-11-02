c34765e5b770d82e81f2b545091c51185f782a34
Dan Niculae <dan.niculae@logichainge.com>
2023-11-02 10:41:35 +0200

    test for fixing db connections

--- PlatePalDjango/settings.py
+++ PlatePalDjango/settings.py
@@ -95,17 +95,17 @@ WSGI_APPLICATION = 'PlatePalDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'dj_db_conn_pool.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db49ukjrioveda',
         'USER': 'njqcdnhutfygdx',
         'PASSWORD': 'fe6ff6791cef7272819e042052a553a3edd7d94dcdc48ee9438f7932ce508319',
         'HOST': 'ec2-52-50-90-145.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
-		'POOL_OPTIONS' : {
-            'POOL_SIZE': 2,
-            'MAX_OVERFLOW': 2,
-            'RECYCLE': 24 * 60 * 60
-        }
+		# 'POOL_OPTIONS' : {
+        #     'POOL_SIZE': 2,
+        #     'MAX_OVERFLOW': 2,
+        #     'RECYCLE': 24 * 60 * 60
+        # }
     }
 }
 
@@ -117,7 +117,7 @@ DATABASES = {
 # }
 
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=0)
 DATABASES['default'].update(db_from_env)
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
