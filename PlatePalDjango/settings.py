a21ebb69faca32dcad13dc9eaadc670256075270
Dan Andrei Niculae <dan.niculae@logichainge.com>
2024-05-03 21:59:20 +0300

    added menu.platepal.eu to allowed hosts

--- PlatePalDjango/settings.py
+++ PlatePalDjango/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 CORS_ALLOWED_ORIGINS = (
     'http://localhost:3000',
 	'https://admin.platepal.eu',
+	'https://menu.platepal.eu',
     'https://platepal.eu',
     'https://www.platepal.eu',
     'https://platepal.eu.',
@@ -39,9 +40,9 @@ CORS_ALLOWED_ORIGINS = (
 
 CORS_ALLOW_CREDENTIALS = True
 
-ALLOWED_HOSTS = ['plate-pal-97cd0667892d.herokuapp.com', 'localhost', 'platepal.eu', 'www.platepal.eu', 'platepal.eu.', 'admin.platepal.eu']
+ALLOWED_HOSTS = ['plate-pal-97cd0667892d.herokuapp.com', 'localhost', 'platepal.eu', 'www.platepal.eu', 'platepal.eu.', 'admin.platepal.eu', 'menu.platepal.eu']
 
-CSRF_TRUSTED_ORIGINS = ['https://plate-pal-97cd0667892d.herokuapp.com', 'https://platepal.eu', 'https://www.platepal.eu', 'https://platepal.eu.', 'http://localhost:3000', 'https://admin.platepal.eu']
+CSRF_TRUSTED_ORIGINS = ['https://plate-pal-97cd0667892d.herokuapp.com', 'https://platepal.eu', 'https://www.platepal.eu', 'https://platepal.eu.', 'http://localhost:3000', 'https://admin.platepal.eu', 'https://menu.platepal.eu']
 
 
 # Application definition
